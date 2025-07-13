9c263423fd70c69cd6c0c7a9812f1084482c6d9a
Ashifaachi <ashifaachi300@gmail.com>
2025-07-13 12:49:35 +0530

    Create serializers for Product and Review

---
+++ reviews/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Product, Review
+from django.contrib.auth.models import User
+
+# ----------------------------------------
+# Review Serializer
+# ----------------------------------------
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'user', 'rating', 'feedback']
+
+    def validate_rating(self, value):
+        if not 1 <= value <= 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
+
+# ----------------------------------------
+# Product Serializer
+# ----------------------------------------
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    average_rating = serializers.FloatField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'average_rating', 'reviews']
+
+# ----------------------------------------
+# User Registration Serializer
+# ----------------------------------------
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
